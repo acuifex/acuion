f13f26b5cbd39cf949bf3d5a53d0d80663ff4368
acuifex <vatrushkaschaem@gmail.com>
2019-10-05 20:44:07 +0300

    failed try in getting user messages.

--- src/Hooks/DispatchUserMessage.cpp
+++ src/Hooks/DispatchUserMessage.cpp
@@ -2,16 +2,27 @@
 
 #include "../interfaces.h"
 
+#include "../SDK/bitbuf.h"
+
 
 typedef void (*DispatchUserMessageFn) (void*, unsigned int, unsigned int, unsigned int, const void*);
 
 void Hooks::DispatchUserMessage(void* thisptr, unsigned int msg_type, unsigned int unk1, unsigned int nBytes, const void* msg_data)
 {
-	switch (msg_type)
-	{
-	case 46: // vote start
-		break;
-	}
 
 	clientVMT->GetOriginalMethod<DispatchUserMessageFn>(38)(thisptr, msg_type, unk1, nBytes, msg_data);
+
+//	if(msg_type == 6){
+//		bf_read read = bf_read(reinterpret_cast<uintptr_t>(msg_data));
+//		read.setOffset(1);
+//		int ent_index = read.readByte();
+//		read.skip(3);
+//		std::string msg_name = read.readString();
+//		std::string player_name = read.readString();
+//		std::string message = read.readString();
+//		std::string location = read.readString();
+//		std::string unk0 = read.readString();
+//		bool all_chat = read.readBool();
+//		cvar->ConsoleDPrintf("ent_index: %d \n msg_name: %s \n player_name: %s \n message: %s \n location: %s \n unk0: %s \n all_chat (boolean): %d \n", ent_index, msg_name, player_name, message, location, unk0, all_chat);
+//	}
 }
--- src/SDK/bitbuf.h
+++ src/SDK/bitbuf.h
@@ -12,3 +12,56 @@ public:
     char pad_001A[2]; //0x001A
     char* m_pDebugName; //0x001C
 }; //Size: 0x0024
+
+class bf_read // because fuck you. i'm lazy
+{
+public:
+	
+	uintptr_t base_address;
+	uintptr_t cur_offset;
+
+	bf_read(uintptr_t addr)
+	{
+		base_address = addr;
+		cur_offset = 0;
+	}
+
+	void setOffset(uintptr_t offset)
+	{
+		cur_offset = offset;
+	}
+
+	void skip(uintptr_t length)
+	{
+		cur_offset += length;
+	}
+
+	int readByte()
+	{
+		auto val = *reinterpret_cast<char*>(base_address + cur_offset);
+		++cur_offset;
+		return val;
+	}
+
+	bool readBool()
+	{
+		auto val = *reinterpret_cast<bool*>(base_address + cur_offset);
+		++cur_offset;
+		return val;
+	}
+
+	std::string readString()
+	{
+		char buffer[256];
+
+		auto str_length = *reinterpret_cast<char*>(base_address + cur_offset);
+		++cur_offset;
+
+		std::memcpy(buffer, reinterpret_cast<void*>(base_address + cur_offset), str_length > 255 ? 255 : str_length);
+
+		buffer[str_length > 255 ? 255 : str_length] = '\0';
+		cur_offset += str_length + 1;
+
+		return std::string(buffer);
+	}
+};
